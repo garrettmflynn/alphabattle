b514c4cbd444d3052864d1e804fb95a51600a090
System Administrator <root@Users-MacBook-Pro-1977.local>
2021-02-21 23:42:01 -0800

    I changed the stop function to be a fade out and make some other small modifications.

--- audio.js
+++ audio.js
@@ -2,7 +2,7 @@
 //This contains all data structures for the 
 //audio functionality in the Brain Battle game
 class sfx {
-  constructor(root){
+  constructor(root, nVel) {
     this.alert = [new p5.MonoSynth(), new p5.MonoSynth(), new p5.MonoSynth(), new p5.MonoSynth()];
     for (var i = 0; i < 4; i++)
       this.alert[i].setADSR(0.001, 0.3, 0.9, 0.08);
@@ -10,14 +10,14 @@ class sfx {
     this.volMain = 1;
   }
   
-  playAlert(nVel, volMain) {
+  playAlert(volMain) {
     this.alert[0].play(this.root * (1.06 / 2));
     this.alert[1].play(this.root * (1.06), this.volMain * this.nVel / 3);
     this.alert[2].play(this.root * (2 * 1.06), this.volMain * this.nVel / 6);
     this.alert[3].play(this.root * (4 * 1.06), this.volMain * this.nVel / 8);
   }
   
-  setVolume(volMain, nVel){ 
+  setVolume(volMain){ 
    this.alert[0].amp(this.volMain * this.nVel, 0.01);
    this.alert[1].amp(volMain * this.nVel / 3, 0.01)
    this.alert[2].amp(this.volMain * this.nVel / 6, 0.01);
@@ -26,10 +26,11 @@ class sfx {
 }
 
 class music {
-  constructor(root){
-    userStartAudio();
-    //programming the drone:
-    this.volMain = 0.5;
+  constructor(root, volMain){
+//  userStartAudio();
+//  getAudioContext.resume();
+    //programming the droneA
+    this.volMain = volMain;
     this.root = root;
     this.drone = [new p5.Oscillator('sine'), new p5.Oscillator('sine'), new p5.Oscillator('sine'), new p5.Oscillator('sine')];
   }
@@ -43,20 +44,20 @@ class music {
       this.drone[k].amp(0); 
   }
   
-  stop() {
-    for(var k=0; k<4; ++k)
-      this.drone[k].stop();
-  }
-  
-   setVolume(volMain) {
-     this.drone[0].amp(this.volMain * 0.125, 0.2);
-     this.drone[1].amp(this.volMain * 0.08, 0.2);
-     this.drone[2].amp(this.volMain * 0.06, 0.2);
-     this.drone[3].amp(this.volMain * 0.075, 0.2);
+   setVolume(volCurr) {
+     this.drone[0].amp(volCurr * 0.125, 0.2);
+     this.drone[1].amp(volCurr * 0.08, 0.2);
+     this.drone[2].amp(volCurr * 0.06, 0.2);
+     this.drone[3].amp(volCurr * 0.075, 0.2);
    }
   
+  stop(volMain) {
+    for(var fade = 1; fade > 0; fade -= pow(10, -4))
+        this.setVolume(fade * volMain);
+  }
+  
    updateDetune(health, healthInit){
-     if (this.health <= healthInit) {
+     if (this.health <= this.healthInit) {
      this.drone[1].freq(2 * this.root + 10 * (1 - exp(-(1 / 50) * (this.healthInit - this.health) / 0.4)) + 0.125);
      this.drone[2].freq(3*this.root + 4*(1-exp(-(1/50)*(this.healthInit - this.health)/0.4)) - 0.125);
    }
